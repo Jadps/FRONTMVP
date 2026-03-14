53892b5dde53fbd8ff49138dea820ae981cc1889
Alonsoprogamer <jesusalodominguez.jadp@gmail.com>
2026-03-14 11:55:09 -0600

    feat: Sistema en ingles y preparaciones para cargar modulos por rol y permisos por rol sobre los modulos

--- src/app/core/models/rol.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface RolDTO {
-    id?: string;
-    name: string;
-    descripcion?: string | null;
-    tenantId?: number | null;
-}
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -4,22 +4,19 @@ import { BehaviorSubject, catchError, map, Observable, of, switchMap, tap } from
 import { environment } from '../../../environments/environment';
 import { SILENCE_ERRORS } from '../interceptors/http-context.tokens';
 
-import { UsuarioDTO } from '../models/usuario.dto';
-export interface LoginCredentials {
-    email: string;
-    password?: string;
-}
+import { UserDto } from '../models/user.dto';
+import { LoginDto } from '../models/auth.dto';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    public currentUser = signal<UsuarioDTO | null>(null);
+    public currentUser = signal<UserDto | null>(null);
     public isRefreshing = signal<boolean>(false);
     public refreshTokenSubject = new BehaviorSubject<boolean | null>(null);
     private readonly http = inject(HttpClient);
 
-    login(credentials: LoginCredentials): Observable<boolean> {
+    login(credentials: LoginDto): Observable<boolean> {
         return this.http.post<any>(`${environment.apiUrl}/auth/login`, credentials, { withCredentials: true }).pipe(
             switchMap(() => {
                 return this.getProfile();
@@ -51,7 +48,7 @@ export class AuthService {
     }
 
     getProfile(): Observable<boolean> {
-        return this.http.get<UsuarioDTO>(`${environment.apiUrl}/usuarios/me`, {
+        return this.http.get<UserDto>(`${environment.apiUrl}/users/me`, {
             withCredentials: true,
             context: new HttpContext().set(SILENCE_ERRORS, true)
         }).pipe(
