6b94e7efb864b87b41d16113a87fe11564771a3a
Jadps <jadp.xs@gmail.com>
2026-03-28 23:20:30 -0600

    fix: Hub url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.alonsodev.online/api/v1.0'
+    apiUrl: 'https://api.alonsodev.online/api/v1.0',
+    hubUrl: 'https://api.alonsodev.online/hubs'
 };
